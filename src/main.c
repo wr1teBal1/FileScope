170f4227d84363e41310e3c5d977ac1c7341ff79
Wr1te <937313051@qq.com>
2025-05-21 18:33:07 +0800

    修改md文件

--- src/main.c
+++ src/main.c
@@ -2,22 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main(int argc, char *argv[]) {
-    if (argc < 2) {
-        fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
-        return 1;
-    }
-
-    FILE *file = fopen(argv[1], "r");
-    if (!file) {
-        perror("Failed to open file");
-        return 1;
-    }
-
-    char line[256];
-    while (fgets(line, sizeof(line), file)) {
-        printf("%s", line);
-    }
-    fclose(file);
+int main() {
+    printf("Hello, World!\n");
     return 0;
 }
